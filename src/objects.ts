451ff912a1c91fc25499baf996c6cd69d4d986f7
Amar <amarrman@udel.edu>
2026-02-23 13:07:54 -0500

    fixed_conditional_error

--- src/objects.ts
+++ src/objects.ts
@@ -49,14 +49,10 @@ export function isCorrect(question: Question, answer: string): boolean {
  * be exactly one of the options.
  */
 export function isValid(question: Question, answer: string): boolean {
-    if (question.type === "short_answer_question") {
-        return true;
-    }
-    if (question.type === "multiple_choice_question") {
-        return question.options.includes(answer);
-    }
-
-    return false;
+    return (
+        question.type === "short_answer_question" ||
+        question.options.includes(answer)
+    );
 }
 
 /**
